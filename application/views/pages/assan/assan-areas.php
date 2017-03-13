ed935617e95039bbf15b267082310e03a58faac7
mizraklar <mizraklar@hotmail.com>
2017-03-14 00:04:59 +0300

    veriTakip
    Endüstri 4.0 Nesneler ile haberleşme sistemleri

--- application/views/pages/assan/assan-areas.php
+++
@@ -1,9 +0,0 @@
-<div class="col-md-12 col-sm-12 col-xs-12 col-lg-12" style="background: url(<?php echo base_url('assets/img/assanlogo/sicpa-assan-pdf1.png'); ?>) repeat-x fixed; background-position: center;">
-    <?php 
-    require_once 'assan-ankara.php';
-    require_once 'assan-mersin.php';
-    require_once 'assan-izmir.php';
-    //require_once 'assan-denizli.php';        
-    //require_once 'assan-tekirdag.php';
-    ?>
-</div>
